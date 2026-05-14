de822100e924b23edddc170370c812d0741f6790
unknown <opadips@gmail.com>
2026-05-14 16:32:24 +0330

    update start.bat

--- start.bat
+++ start.bat
@@ -1,35 +1,25 @@
 @echo off
-title Holyroom - First Launch & Start
-
-:: Store the root directory of the project
+title Holyroom Launcher
 set ROOT=%~dp0
 
-:: Check and install server dependencies if needed
+echo Checking dependencies...
+
 if not exist "%ROOT%server\node_modules" (
-    echo.
-    echo [INFO] Installing server dependencies...
+    echo Installing server dependencies...
     cd /d "%ROOT%server"
     call npm install
     cd /d "%ROOT%"
 )
 
-:: Check and install client dependencies if needed
 if not exist "%ROOT%client\node_modules" (
-    echo.
-    echo [INFO] Installing client dependencies...
+    echo Installing client dependencies...
     cd /d "%ROOT%client"
     call npm install
     cd /d "%ROOT%"
 )
 
-:: Start the server and client
-echo.
-echo [INFO] Starting Holyroom Server...
+echo Starting server and client...
 start "Holyroom Server" cmd /k "cd /d "%ROOT%server" && npm run dev"
-
-echo [INFO] Starting Holyroom Client...
 start "Holyroom Client" cmd /k "cd /d "%ROOT%client" && npm start"
 
-echo.
-echo All done! Both windows should open shortly.
-pause >nul
+exit
